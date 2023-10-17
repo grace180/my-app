9a5e71ddb1eb567c8db1c4d6ab46fa30a63376b6
grace180 <anugra123mariani@gmail.com>
2023-10-18 04:11:47 +0800

    week4

---
+++ src/Accordion.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+
+export default function AccordionItem({ label, content }) {
+    const [isActive, setIsActive] = useState(false);
+  
+    const toggleAccordion = () => {
+      setIsActive(!isActive);
+    };
+  
+    return (
+      <div className={`detail-item ${isActive ? 'active' : ''}`}>
+        <div className="detail-label" onClick={toggleAccordion}>
+          {label}
+          <span className={isActive ? "minus" : "plus"}>{isActive ? "-" : "+"}</span>
+        </div>
+        {isActive && <span className="detail-value">{content}</span>}
+      </div>
+    );
+  }
+ 
+  
--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState } from 'react';
+import anugrah from './anugrah.jpg';
 
-function App() {
+const App = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  const [activeAccordion, setActiveAccordion] = useState(null);
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+  const handleAccordionClick = (index) => {
+    setActiveAccordion(activeAccordion === index ? null : index);
+  };
+
+  const accordionData = [
+    { title: 'Jurusan', content: 'Informatika ' },
+    { title: 'Jalur Sukses', content: 'Corporate Entreprenuership' },
+    { title: 'Angkatan', content: '2022' },
+    { title: 'Hobby', content: 'Nonton drakor dan Tidur' },
+  ];
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><div className="app">
+      <div className="container">
+        <div className="sisiKiri">
+          <div className="imageContainer">
+            <img src="./anugrah.jpg" alt="Profil"
+              style={{ display: isVisible ? 'block' : 'none' }} />
+            <button onClick={toggleVisibility}>Toggle Image</button>
+          </div>
+        </div>
+        <div className="sisiKanan">
+          <div className="accordion">
+            {accordionData.map((item, index) => (
+              <div key={index} className="accordionItem">
+                <div className="accordion-title" onClick={() => handleAccordionClick(index)}>
+                  {item.title}
+                </div>
+                {activeAccordion === index && (
+                  <div className="accordionContent">{item.content}</div>
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+    </div><div className='Organisasi'>
+        <h2>Riwayat Organisasi</h2>
+        <p className='organisasii'>Informatika Student Union</p>
+      </div></>
   );
-}
+};
 
 export default App;
